ee1ab296a857c3d39c1d480ff3b81a58094cf620
Sahatsachai1166109060796 <156157261+Asis1166109060796@users.noreply.github.com>
2024-02-12 21:44:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-def plus():
+def plus():#สหัสชัย สิทธิมงคล
             a = float(input(": "))
             b = float(input(": "))
             ผลลัพธ์ = a + b
             print(ผลลัพธ์)
 
-print("บวก กด 1")
-print("ลบ กด 2")
-print("คูณ กด 3")
+while True:
+    print("บวก กด 1")
+    print("ลบ กด 2")
+    print("คูณ กด 3")
+    print("หาร กด 4")
 
-choose=int(input("กรอกวิธีคำนวณ"))
-if (choose==1):
+    choose=int(input("กรอกวิธีคำนวณ"))
+    if (choose==1):
         plus()
-else:
+    else:
         print("error")
